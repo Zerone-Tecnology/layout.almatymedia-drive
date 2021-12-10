9cf5921c96a168a839b06fc9ce0aa552b644b4ed
MordecaiOhhh <44517579+MordecaiOhhh@users.noreply.github.com>
2021-12-10 15:02:45 +0600

    complex code

---
+++ assets/js/common.js
@@ -0,0 +1,68 @@
+// SEARCH PAGE
+$(document).ready(function () {
+    $('#slider-range').slider({
+        range: true,
+        min: new Date('2020.09.31').getTime() / 1000,
+        max: new Date('2022.02.31').getTime() / 1000,
+        step: 86400,
+        values: [
+            new Date('2021.02.18').getTime() / 1000,
+            new Date('2021.09.09').getTime() / 1000,
+        ],
+        slide: function (event, ui) {
+            $('#amount-1').val(
+                new Date(ui.values[0] * 1000).getFullYear() +
+                ' ' +
+                (new Date(ui.values[0] * 1000).getMonth() + 1) +
+                ' ' +
+                new Date(ui.values[0] * 1000).getDate()
+            );
+            $('#amount-2').val(
+                new Date(ui.values[1] * 1000).getFullYear() +
+                ' ' +
+                (new Date(ui.values[1] * 1000).getMonth() + 1) +
+                ' ' +
+                new Date(ui.values[1] * 1000).getDate()
+            );
+        },
+    });
+    $('#amount-1').val(
+        new Date(
+            $('#slider-range').slider('values', 0) * 1000
+        ).getFullYear() +
+        ' ' +
+        (new Date(
+            $('#slider-range').slider('values', 0) * 1000
+        ).getMonth() +
+            1) +
+        ' ' +
+        new Date($('#slider-range').slider('values', 0) * 1000).getDate()
+    );
+    $('#amount-2').val(
+        new Date(
+            $('#slider-range').slider('values', 1) * 1000
+        ).getFullYear() +
+        ' ' +
+        (new Date(
+            $('#slider-range').slider('values', 1) * 1000
+        ).getMonth() +
+            1) +
+        ' ' +
+        new Date($('#slider-range').slider('values', 1) * 1000).getDate()
+    );
+});
+
+$('.open_btn, .filter_search-block-open-btn').click(function () {
+    $('.open_btn').toggleClass('close');
+    $('.filter_search-block-open-btn').toggleClass('close');
+    $('.filter--block_open').toggleClass('open');
+    $('.filter_search-block').toggleClass('open');
+});
+$('.filter--block_open, .filter_search-block-close-btn').click(
+    function () {
+        $('.open_btn').toggleClass('close');
+        $('.filter_search-block-open-btn').toggleClass('close');
+        $('.filter--block_open').toggleClass('open');
+        $('.filter_search-block').toggleClass('open');
+    }
+);
